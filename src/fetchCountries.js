139e0a07367c14f982e3f108654944446018bee2
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-02 20:38:03 +0100

    task10 - change5

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export async function fetchCountries(name) {
-    const baseUrl = 'https://restcountries.com/v3.1/name/{name}';
+    const baseUrl = 'https://restcountries.com/v3.1/name/';
     const response = await fetch(`${baseUrl}${name}?fields=name,name.official,capital,population,flags.svg,languages`);
     
     if (!response.ok) {
