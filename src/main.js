b69e037a70512df7b81e3c306a4cc0c77633cf49
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-02 23:08:58 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,72 +1,64 @@
-import fetchCountries from './fetchCountries.js';
-import debounce from 'lodash/debounce';
+// main.js
+import { fetchCountries } from './fetchCountries.js';
 import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
 
 const searchBox = document.getElementById('search-box');
-const message = document.getElementById('message');
 const countryList = document.getElementById('country-list');
 const countryInfo = document.getElementById('country-info');
 
-const DEBOUNCE_DELAY = 300;
+searchBox.addEventListener('input', _.debounce(onSearch, 300));
 
-searchBox.addEventListener('input', debounce(searchCountries, DEBOUNCE_DELAY));
-
-async function searchCountries(countries) {
-  const query = searchBox.value.trim();
-
-  if (query === '') {
-    clearResults();
-    return;
-  }
-
-  try {
-    const countries = await fetchCountries(query);
-
-    if (countries.length > 10) {
-      Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-      clearResults();
-    } else if (countries.length >= 2 && countries.length <= 10) {
-      renderCountryList(countries);
-      clearCountryInfo();
-    } else if (countries.length === 1) {
-      renderCountryInfo(countries[0]);
-    } else {
-      Notiflix.Notify.failure('Oops, there is no country with that name.');
-      clearResults();
+function onSearch() {
+    const searchQuery = searchBox.value.trim();
+    
+    if (searchQuery === '') {
+        clearResults();
+        return;
     }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    Notiflix.Notify.failure('Error fetching data. Please try again.');
-  }
+
+    fetchCountries(searchQuery)
+        .then((countries) => {
+            if (countries.length === 1) {
+                displayCountryInfo(countries[0]);
+            } else if (countries.length >= 2 && countries.length <= 10) {
+                displayCountryList(countries);
+            } else if (countries.length > 10) {
+                notiflix.Notify.failure('Too many matches found. Please enter a more specific name.');
+                clearResults();
+            } else {
+                notiflix.Notify.failure('Oops, there is no country with that name.');
+                clearResults();
+            }
+        })
+        .catch((error) => {
+            notiflix.Notify.failure('Oops, there is no country with that name.');
+            clearResults();
+        });
 }
 
 function clearResults() {
-  message.innerHTML = '';
-  countryList.innerHTML = '';
-  clearCountryInfo();
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
 }
 
-function clearCountryInfo() {
-  countryInfo.innerHTML = '';
+function displayCountryList(countries) {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+    countries.forEach((country) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `<img src="${country.flags.svg}" alt="${country.name.official}"> ${country.name.official}`;
+        countryList.appendChild(listItem);
+    });
 }
 
-function renderCountryList(countries) {
-  countryList.innerHTML = '';
-  countries.forEach((country) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<img src="${country.flags.svg}" alt="${country.name.official}"> ${country.name.official}`;
-    listItem.addEventListener('click', () => renderCountryInfo(country));
-    countryList.appendChild(listItem);
-  });
-}
-
-function renderCountryInfo(country) {
-  message.innerHTML = '';
-  countryList.innerHTML = '';
-  const flag = `<img src="${country.flags.svg}" alt="${country.name.official}">`;
-  const infoHTML = `<h2>${country.name.official}</h2>
-    <p>Capital: ${country.capital}</p>
-    <p>Population: ${country.population}</p>
-    <p>Languages: ${country.languages.map((lang) => lang.name).join(', ')}</p>`;
-  countryInfo.innerHTML = `${flag}${infoHTML}`;
+function displayCountryInfo(country) {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = `
+        <img src="${country.flags.svg}" alt="${country.name.official}">
+        <h2>${country.name.official}</h2>
+        <p>Capital: ${country.capital}</p>
+        <p>Population: ${country.population.toLocaleString()}</p>
+        <p>Languages: ${country.languages.map(lang => lang.name).join(', ')}</p>
+    `;
 }
