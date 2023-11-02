961f4521b5fa1d50ed61fc5f902294ba1583cf31
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-02 19:46:11 +0100

    task10 - change1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,13 @@
-// fetchCountries.js
 export async function fetchCountries(name) {
-    const baseUrl = 'https://restcountries.com/v2/name/';
+    const baseUrl = 'https://restcountries.com/v3/name/';
     const response = await fetch(`${baseUrl}${name}?fields=name,official,capital,population,flags.svg,languages`);
     
     if (!response.ok) {
+        console.error('Error response:', response.status, response.statusText);
         throw new Error('Country not found');
     }
 
     const data = await response.json();
-    console.log(data);
+    console.log('API Response:', data);
     return data;
 }
