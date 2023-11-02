b573b71db62291ccbb688ea7992872565281aeb2
FrostyJFox <132783113+FrostyJFox@users.noreply.github.com>
2023-11-02 22:58:34 +0100

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,18 +1,8 @@
-// fetchCountries.js
-export async function fetchCountries(name) {
-    const baseUrl = 'https://restcountries.com/v3/name/';
-    const response = await fetch(`${baseUrl}${name}?fields=name.official,capital,population,flags.svg,languages`);
-
-    if (!response.ok) {
-        console.error('Error response:', response.status, response.statusText);
-        throw new Error('Country not found');
-    }
-
-    const data = await response.json();
-
-    if (data.length === 1) {
-        return data[0]; // Return the first (and only) item if there's only one country
-    } else {
-        return data; // Return the array if there are multiple countries
-    }
+export default async function fetchCountries(name) {
+  const response = await fetch(`https://restcountries.com/v2/all/${name}?fields=name,name.official,capital,population,flags.svg,languages`);
+  if (!response.ok) {
+    throw new Error(`Request failed with status ${response.status}`);
+  }
+  const data = await response.json();
+  return data;
 }
